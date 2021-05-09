0213458528942410fa128d780ab7f6850a4a0dbc
saidulislam <saidul.islam@gmail.com>
2021-05-09 17:43:38 -0400

    udacity submitted version

--- web/app/routes.py
+++ web/app/routes.py
@@ -66,25 +66,17 @@ def notification():
         try:
             db.session.add(notification)
             db.session.commit()
-
+            db.session.refresh(notification)
             ##################################################
             ## TODO: Refactor This logic into an Azure Function
             ## Code below will be replaced by a message queue
             #################################################
-            attendees = Attendee.query.all()
-
-            for attendee in attendees:
-                subject = '{}: {}'.format(attendee.first_name, notification.subject)
-                send_email(attendee.email, subject, notification.message)
-
-            notification.completed_date = datetime.utcnow()
-            notification.status = 'Notified {} attendees'.format(len(attendees))
-            db.session.commit()
-            # TODO Call servicebus queue_client to enqueue notification ID
-
-            #################################################
-            ## END of TODO
-            #################################################
+            m = '{}'.format(notification.id)
+            try:
+                msg = Message(m)
+                result = queue_client.send(msg)
+            except Exception as e:
+                logging.error('An error has ocurred when sending msg to queue {}', str(e))
 
             return redirect('/Notifications')
         except :
@@ -96,7 +88,7 @@ def notification():
 
 
 def send_email(email, subject, body):
-    if not app.config.get('SENDGRID_API_KEY')
+    if not app.config.get('SENDGRID_API_KEY'):
         message = Mail(
             from_email=app.config.get('ADMIN_EMAIL_ADDRESS'),
             to_emails=email,
